1c43bdae2ba7238a0928b142891d54fe4ede7c23
Pedro Moreira <pedromoreirah3@gmail.com>
2024-04-25 18:22:35 +0200

    problems with nodemailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,4 @@
-import nodemailer from "modemailer";
+// import nodemailer from "modemailer";
 
 export default async (req, res) => {
   const {name, email, message} = req.body
@@ -6,28 +6,29 @@ export default async (req, res) => {
   const data = {
     name, email, message
   }
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    }
-  });
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: process.env.EMAIL_USERNAME,
+  //     pass: process.env.EMAIL_PASSWORD,
+  //   }
+  // });
 
   try {
-    const mail = await transporter.sendEmail({
-      from: user,
-      to: "pedromoreirah3@gmail.com",
-      replyTo: email,
-      subject: `Contact form submission from ${name}`,
-      html: `
-      <p>Name ${name}</p>
-      <p>Email ${email}</p>
-      <p>Message ${message}</p>
-      `
-    })
+    // const mail = await transporter.sendEmail({
+    //   from: user,
+    //   to: "pedromoreirah3@gmail.com",
+    //   replyTo: email,
+    //   subject: `Contact form submission from ${name}`,
+    //   html: `
+    //   <p>Name ${name}</p>
+    //   <p>Email ${email}</p>
+    //   <p>Message ${message}</p>
+    //   `
+    // })
+    console.log("There's nothing happening here")
   } catch (error) {
     console.log(error)
     res.status(500).json({message: "Could not send email. Your message was not sent."});
